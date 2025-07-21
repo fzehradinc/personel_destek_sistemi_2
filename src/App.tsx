2bbab74790e38a4bf59224f11209263e7869a828
fzehradinc <fzehradinc00@gmail.com>
2025-07-21 12:27:28 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -480,6 +480,7 @@ const AdminDashboard = React.memo(() => {
                       </p>
                     </div>
                   </>
+                )}
               </div>
               
               <div className="flex items-center gap-3">
